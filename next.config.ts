1ab4ab52974207bfa1965b5559a62fa3e346b7d1
dan <893100849@qq.com>
2026-08-14 14:43:00 +0800

    chore: poll for development file changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.31.70"],
   output: "standalone",
   reactStrictMode: true,
+  watchOptions: {
+    pollIntervalMs: 1_000,
+  },
 };
 
 export default nextConfig;
